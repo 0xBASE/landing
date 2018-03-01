3187c4c83d2eb8bf369ef6775a7214edb21bd470
Klemen Kastelic <klemen.kast@gmail.com>
2018-03-01 22:47:11 +0100

    Changed mailchimp url to https

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ $('#join-community').submit(function(e) {
 
   $.ajax({
     type: "GET",
-    url: "http://0xbase.us17.list-manage.com/subscribe/post-json?u=0a96690fa626677a72de0139e&amp;id=e91a754e09&c=?",
+    url: "https://0xbase.us17.list-manage.com/subscribe/post-json?u=0a96690fa626677a72de0139e&amp;id=e91a754e09&c=?",
     data: $('#join-community').serialize(),
     dataType: 'jsonp',
     contentType: "application/json; charset=utf-8",
