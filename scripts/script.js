f2bf351a3f555f8bffb33338f5edb9533c5e4e10
Klemen Kastelic <klemen.kast@gmail.com>
2018-03-01 22:44:58 +0100

    Fixed mailchimp

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,21 @@ $('#join-community').submit(function(e) {
   e.preventDefault();
 
   $.ajax({
-    url: 'https://0xbase.us17.list-manage.com/subscribe/post?u=0a96690fa626677a72de0139e&id=e91a754e09&c=?',
-    type: 'GET',
+    type: "GET",
+    url: "http://0xbase.us17.list-manage.com/subscribe/post-json?u=0a96690fa626677a72de0139e&amp;id=e91a754e09&c=?",
     data: $('#join-community').serialize(),
     dataType: 'jsonp',
     contentType: "application/json; charset=utf-8",
     success: function (data) {
-      if (data['result'] != "success") {
-        console.error(data['msg']);
+      if (data.result === 'success') {
+        $('#join-community').addClass("sent");
       } else {
-        $(this).addClass("sent");
+        $('#join-community').addClass("error");
+        $('.join-error').html(data.msg);
+
+        setTimeout(function(){
+          $('#join-community').removeClass("error");
+        }, 2000);
       }
     }
   });
